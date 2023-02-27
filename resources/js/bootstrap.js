f3e665daf034551da45e1ad0796a5e830075ef22
Mohamed KB <mohabel98@gmail,com>
2023-02-27 21:10:05 +0100

    registro login hecho

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -4,9 +4,10 @@
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-import '../css/app.css'
-import * as bootstrap from 'bootstrap'
+import '../css/app.css';
+import * as bootstrap from 'bootstrap';
 
+import * as logout from './logout';
 import axios from 'axios';
 window.axios = axios;
 
--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,7 +1,8 @@
 <nav class="navbar navbar-expand-lg bg-light">
     <div class="container-fluid">
         <a class="navbar-brand" href="{{route('home')}}">Rapido.es</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -16,7 +17,8 @@
                     <a class="nav-link active" href="#">Donde estamos</a>
                 </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"aria-expanded="false">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">
                         Categorias
                     </a>
                     <ul class="dropdown-menu">
@@ -28,6 +30,26 @@
                         <li><a class="dropdown-item" href="#">Electrodomesticos</a></li>
                     </ul>
                 </li>
+
+                @guest
+                    @if (Route::has ('login'))
+                    <li class="nav-item">
+                        <a class="nav-link" href=" 'login')}}"><span>Entrar</span></a>
+                    </li>
+                    @endif
+                    @if (Route::has('register'))
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{route( 'register')}}"><span>Registrar</span></a>
+                    </li>
+                    @endif
+                @else
+                    <li class="nav-item">
+                        <form id="logoutForm" action="{{ route('logout')}}" method="POST">
+                            @csrf
+                        </form>
+                        <a id="logoutBtn" class="nav-link" href="#">Salir</a>
+                    </li>
+                @endguest
             </ul>
             <form class="d-flex" role="search">
                 <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
