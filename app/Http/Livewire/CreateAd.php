4849c547fa4a66e723a7c53cae89b4d85619faba
Alicia <www.alicia22@gmail.com>
2023-02-28 21:22:46 +0100

    anuncio1

--- app/Http/Livewire/CreateAd.php
+++ app/Http/Livewire/CreateAd.php
@@ -1,17 +1,42 @@
 <?php
 
 namespace App\Http\Livewire;
+use App\Models\Ad;
 
 use Livewire\Component;
 
 class CreateAd extends Component
 {
+    public $title;
+    public $body;
+    public $price;
+    
+    public function store()
+    {
+       Ad::create([
+        'title'=>$this->title,
+        'body'=>$this->body,
+        'price'=>$this->price,
+
+
+       ]);
+
+       $this->cleanForm();
+
+    }
+
+    public function cleanForm()
+    {
+        $this->title = "";
+        $this->body = "";
+        $this->price = "";
+
+
+    }
+
     public function render()
     {
         return view('livewire.create-ad');
     }
 }
 
-<div>
-    <h1>Create new AD </h1>
-</div>
