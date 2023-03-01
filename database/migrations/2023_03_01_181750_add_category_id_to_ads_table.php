97537cc935c010b65a85a6523d003c18f69ecd24
Mohamed KB <mohabel98@gmail,com>
2023-03-01 20:21:33 +0100

    cambios

--- database/migrations/2023_03_01_181750_add_category_id_to_ads_table.php
+++ database/migrations/2023_03_01_181750_add_category_id_to_ads_table.php
@@ -3,31 +3,30 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-class AddCategoryIdToAds extends Migration
 
+return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     * 
-     * @return void 
+    /* Run the migrations.
+     *
+     * @return void
      */
     public function up()
     {
         Schema::table('ads', function (Blueprint $table) {
-            $table->unsignedBigInteger('category_id')->default(1);
-            $table->foreing('category_id')->references('id')->on('categories');
-            
+            $table->UnsignedBigInteger('category_id')->default(1);
+            $table->foreign('category_id')->references('id')->on('categories');
         });
     }
 
-    /**
-     * Reverse the migrations.
+    /* Reverse the migrations.
+     *
+     * @return void
      */
     public function down()
     {
         Schema::table('ads', function (Blueprint $table) {
-            $table->dropForeing(['caregory_id']);
-            $table->dropColumn('caregory_id');
+            $table->dropForeign(['category_id']);
+            $table->dropColumn(['category_id']);
         });
     }
 };
