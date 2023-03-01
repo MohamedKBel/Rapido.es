5ed741d38e444f1f407f74bae12585537e6f2459
Mohamed KB <mohabel98@gmail,com>
2023-03-01 20:25:31 +0100

    cambios

--- database/migrations/2023_02_28_202458_create_categories_table.php
+++ database/migrations/2023_02_28_202458_create_categories_table.php
@@ -1,26 +1,32 @@
 <?php
 
+use App\Models\Category;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
+    /* Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table){
             $table->id();
+            $table->string('name');
             $table->timestamps();
         });
+        $categories = [
+            'coches','motos','hogar','electrónica','móviles','ordenadores'
+        ];
+        foreach ($categories as $category) {
+            Category::create(['name'=>$category]);
+        }
     }
 
-    /**
-     * Reverse the migrations.
+    /* Reverse the migrations.
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('categories');
     }
