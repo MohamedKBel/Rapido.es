24357da31403e39bbe9d317c840c248aeffd66f0
Mohamed KB <mohabel98@gmail,com>
2023-03-02 20:33:31 +0100

    cambios

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
-
 Route::get('/ad/create',[AdController::class,'create'])->name('ad.create');
-
-
