e16e98e7f046f71a12588217a80af69b0179fc0d
Alicia <www.alicia22@gmail.com>
2023-03-07 19:26:35 +0100

    visualizacion de categorias

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -11,4 +11,11 @@ class PublicController extends Controller
         $ads = Ad::orderBy('created_at','desc')->take(6)->get();
         return view('welcome',compact('ads'));
     }
+
+    public function adsByCatagory(Category $category){
+        $ads = $category->ads()->get();
+        return view('ad.by-category',compact ('category','ads'));
+    }
+
+
 }
