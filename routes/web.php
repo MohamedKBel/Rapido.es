f75e372f7a0404857961f6ff31fb54dfc4eb5f1b
Mohamed KB <mohabel98@gmail,com>
2023-03-07 19:30:56 +0100

    welcome renovada

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,5 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/',[PublicController::class,'index'])->name('home');
-Route::get('/ad/create',[AdController::class,'create'])->name('ad.create');
+Route::get('/ad/create',[AdController::class,'create'])->name('ads.create');
 
